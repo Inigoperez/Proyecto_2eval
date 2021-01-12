da5bb99ac45639254a01acd78b1ce1016f0e2813
2dam3 <ikbem@plaiaundi.net>
2021-01-12 12:55:22 +0100

    ultimo commit del dia

--- proyecto_2eval/lib/Controllers/LoginController.dart
+++ proyecto_2eval/lib/Controllers/LoginController.dart
@@ -1,8 +1,7 @@
 import 'package:http/http.dart' as http;
-import 'package:proyecto_2eval/views/Route_selection_view.dart';
+import 'package:proyecto_2eval/models/Users.dart';
 
 ///////-- Iniciio función inicio de sesión --/////////
-
 login(String email, String password) async {
   final String url = 'http://10.10.12.133:8080/usuario/login?email=' +
       email +
@@ -12,22 +11,19 @@ login(String email, String password) async {
   final response = await http.get('$url');
   print(response.statusCode);
   if (response.statusCode == 200) {
-    print(response.body);
     if (response.body == 'true') {
-      RouteSelection();
+      print("perfect");
     } else {
       print("No esta registrado");
     }
   } else {
-    print("Error de conexion con la api" + response.statusCode.toString());
+    print("Error de conexion con la api, estado:" +
+        response.statusCode.toString());
   }
 }
+
 ///////-- Cierre función inicio de sesión --//////////
 
-//////////////////////////////////////////////////////
 ////////-- Inicio función de registro --//////////////
-//////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////
 ////////-- Cierre función registro --/////////////////
-//////////////////////////////////////////////////////
