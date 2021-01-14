a50d3afa84321b67c3f3ce1234c248dd30c0ae09
2dam3 <ikbem@plaiaundi.net>
2021-01-14 08:33:44 +0100

    empezado hacer lo de las rutas

---
+++ proyecto_2eval/lib/models/Rutas.dart
@@ -0,0 +1,12 @@
+import 'dart:ffi';
+
+import 'Puntos.dart';
+
+class Rutas {
+  String id;
+  String ciudad;
+  String duracion;
+  Double puntos;
+  String nombre;
+  List<Puntos> lista_puntos;
+}
